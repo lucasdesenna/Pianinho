4ed4a0b80808a3cf2bcd885f96a2dd2ee7f07897
lucasdesenna <lucasdesenna@gmail.com>
2013-01-07 03:40:49 -0200

    note preloarder

--- mechanics.js
+++ mechanics.js
@@ -1,5 +1,23 @@
 var soundStack = 0;
 
+function setupNotes() {
+	var keys = $("li > button")
+	for(var i = 0; i < keys.length; i++) {
+		var note = $(keys).get(i).attr("id");
+		var noteId = "s" + soundStack;
+		var soundObj = "<audio id='" + noteId + "'><source src='wavs/" + note + ".wav' type='audio/wav'>Your browser does not support the audio element.</audio>";
+		manageStack();
+		$("body").append(soundObj);
+		var duration = 2000;
+		setTimeout(function() {
+			$("#" + noteId).remove();
+		}, duration);
+	}
+	$(document).ready(function(e) {
+        $("audio").remove();
+    });
+}
+
 function play(target) {
 	var note = $(target).attr("id");
 	var noteId = "s" + soundStack;
