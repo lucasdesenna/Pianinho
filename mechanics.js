a926eb7efdaad97a975040c0c1169e478686168a
lucasdesenna <lucasdesenna@gmail.com>
2013-01-07 23:25:05 -0200

    preload

--- mechanics.js
+++ mechanics.js
@@ -3,15 +3,12 @@ var soundStack = 0;
 function setupNotes() {
 	var keys = $("li > button")
 	for(var i = 0; i < keys.length; i++) {
-		var note = $(keys).get(i).attr("id");
+		var note = $(keys)[i];
+		note = $(note).attr("id");
 		var noteId = "s" + soundStack;
-		var soundObj = "<audio id='" + noteId + "'><source src='wavs/" + note + ".wav' type='audio/wav'>Your browser does not support the audio element.</audio>";
+		var soundObj = "<audio id='" + noteId + "' preload><source src='wavs/" + note + ".wav' type='audio/wav'>Your browser does not support the audio element.</audio>";
 		manageStack();
 		$("body").append(soundObj);
-		var duration = 2000;
-		setTimeout(function() {
-			$("#" + noteId).remove();
-		}, duration);
 	}
 	$(document).ready(function(e) {
         $("audio").remove();
